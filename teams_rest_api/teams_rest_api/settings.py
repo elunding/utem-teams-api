9ed5d14377fc1501b8a0257ad96292ee58aa5dc8
Esteban Lundin <elundin@techk.cl>
2020-09-26 16:35:13 -0300

    installed psycopg2 and added connection to utem-teams-db

--- teams_rest_api/teams_rest_api/settings.py
+++ teams_rest_api/teams_rest_api/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'teams_rest_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'utem_teams_db',
+        'USER': 'steve',
+        'PASSWORD': 'unknown',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
