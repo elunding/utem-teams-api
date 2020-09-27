125f307aa3dc4756b63c72d4be406be17edccef4
Esteban Lundin <elundin@techk.cl>
2020-09-26 21:06:49 -0300

    added url paths

--- teams_rest_api/teams_rest_api/urls.py
+++ teams_rest_api/teams_rest_api/urls.py
@@ -16,6 +16,79 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from projects.views import (
+    ProjectDetailView,
+    ProjectCreateView,
+    ProjectUpdateView,
+    ProjectDeleteView,
+    ProjectListView,
+)
+
+from tasks.views import (
+    TaskListView,
+    TaskCreateView,
+    change_priority,
+)
+
+from users.views import CustomLoginView
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        'admin/',
+        admin.site.urls,
+    ),
+    # user authentication views
+    path(
+        'login/',
+        CustomLoginView.as_view(),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
+    # projects
+    path(
+        'projects/',
+        ProjectListView.as_view(),
+        name='project-list-view',
+    ),
+    path(
+        'project/create/',
+        ProjectCreateView.as_view(),
+        name='project-create-view',
+    ),
+    path(
+        'project/<int:pk>/delete/',
+        ProjectDeleteView.as_view(),
+        name='project-delete-view',
+    ),
+    path(
+        'project/<int:pk>/update/',
+        ProjectUpdateView.as_view(),
+        name='project-update-view',
+    ),
+    path(
+        'project/<int:pk>/',
+        ProjectDetailView.as_view(),
+        name='project-detail-view',
+    ),
+    # project-tasks views
+    path(
+        'project/<int:pk>/tasks/',
+        TaskListView.as_view(),
+        name='project-tasks-list-view',
+    ),
+    path(
+        'project/<int:pk>/task/create/',
+        TaskCreateView.as_view(),
+        name='task-create-view',
+    ),
+    path(
+        'task/<int:pk>/change_priority/',
+        change_priority,
+        name='task-change-priority',
+    ),
 ]
