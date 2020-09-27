1e5e192154440935661a795195db2c416a326575
Esteban Lundin <elundin@techk.cl>
2020-09-27 16:04:14 -0300

    added projects and tasks serializers

--- teams_rest_api/projects/models.py
+++ teams_rest_api/projects/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Project(models.Model):
     name = models.TextField()
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -11,21 +13,23 @@ class Project(models.Model):
 
 class Task(models.Model):
 
-    class Priority(models.IntegerChoices):
-        LOW = 1
-        MEDIUM = 2
-        HIGH = 3
+    PRIORITY_CHOICES = (
+        ('1', 'LOW'),
+        ('2', 'MEDIUM'),
+        ('3', 'HIGH'),
+    )
 
-    class Status(models.TextChoices):
-        TODO = 'TD',
-        IN_PROGRESS = 'IP',
-        DONE = 'DN',
+    STATUS_CHOICES = (
+        ('TD', 'TODO'),
+        ('IP', 'IN_PROGRESS'),
+        ('DN', 'DONE'),
+    )
 
     name = models.TextField()
     description = models.TextField()
-    priority = models.IntegerField(choices=Priority.choices)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES)
     status = models.CharField(
-        choices=Status.choices,
+        choices=STATUS_CHOICES,
         max_length=2,
         default='TD',
     )
@@ -34,3 +38,8 @@ class Task(models.Model):
         related_name='tasks',
         on_delete=models.CASCADE,
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
---
+++ teams_rest_api/projects/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from . models import (
+    Project,
+    Task,
+)
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.Charfield(
+        required=True,
+        max_length=100,
+    )
+    description = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        max_length=500,
+    )
+    tasks = serializers.StringRelatedField(many=True)
+
+    class Meta:
+        model = Project
+        fields = ['name', 'description', 'created_at', 'updated_at', 'tracks']
+
+
+    def create(self, validated_data):
+        """
+
+        :param validated_data: deserialized data
+        :return: new Project instance
+        """
+        return Project.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+
+        :param instance: Project instance
+        :param validated_data: deserialized data
+        :return: updated instance
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+
+        return instance
+
+
+class TaskSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.Charfield(
+        required=True,
+        max_length=100,
+    )
+    description = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        max_length=500,
+    )
+    priority = serializers.CharField(source='get_priority_display')
+    status = serializers.CharField(source='get_status_display')
+
+    class Meta:
+        model = Task
